cd887ce4d4b0cda0a91063aa57c07af6e24c32ec
Alan Torres <temp_peri@hotmail.com>
2016-08-18 16:58:38 -0500

    Pagina de configuración

---
+++ Cinotam.AbpModuleZero.Web/Areas/SysAdmin/Controllers/ConfigurationController.cs
@@ -0,0 +1,14 @@
+﻿using Cinotam.AbpModuleZero.Web.Controllers;
+using System.Web.Mvc;
+
+namespace Cinotam.AbpModuleZero.Web.Areas.SysAdmin.Controllers
+{
+    public class ConfigurationController : AbpModuleZeroControllerBase
+    {
+        // GET: SysAdmin/Configuration
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
