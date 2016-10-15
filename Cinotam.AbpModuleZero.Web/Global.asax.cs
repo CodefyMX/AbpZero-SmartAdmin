c98d1d1238964ab6908a2527e2965589382759d4
periface <temp_peri@hotmail.com>
2016-10-15 01:24:07 -0500

    Logger deshabilitado

--- Cinotam.AbpModuleZero.Web/Global.asax.cs
+++ Cinotam.AbpModuleZero.Web/Global.asax.cs
@@ -1,5 +1,4 @@
 ﻿using Abp.Web;
-using Castle.Facilities.Logging;
 using System;
 using System.Web.Mvc;
 
@@ -9,7 +8,8 @@ namespace Cinotam.AbpModuleZero.Web
     {
         protected override void Application_Start(object sender, EventArgs e)
         {
-            AbpBootstrapper.IocManager.IocContainer.AddFacility<LoggingFacility>(f => f.UseLog4Net().WithConfig("log4net.config"));
+            //Disabled logger
+            //AbpBootstrapper.IocManager.IocContainer.AddFacility<LoggingFacility>(f => f.UseLog4Net().WithConfig("log4net.config"));
             //find the default JsonVAlueProviderFactory
             JsonValueProviderFactory jsonValueProviderFactory = null;
 
