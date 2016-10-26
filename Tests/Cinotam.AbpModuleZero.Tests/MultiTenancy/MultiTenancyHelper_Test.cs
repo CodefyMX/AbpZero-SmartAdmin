2a45399aa06f9233d0af7e84a1dfb17917ed697f
Alan Torres <aats91@outlook.com>
2016-10-26 00:02:59 -0500

    Agregado MultiTenancyHelper.cs -Se encarga de extraer el nombre de la empresa de la url de la petición. ToDo: encontrar la forma de cambiar el tenantId de forma global (no se si sea correcto), o buscar otra alternativa.

---
+++ Tests/Cinotam.AbpModuleZero.Tests/MultiTenancy/MultiTenancyHelper_Test.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Cinotam.ModuleZero.AppModule.MultiTenancy.MultiTenancyHelper;
+using Shouldly;
+using Xunit;
+
+namespace Cinotam.AbpModuleZero.Tests.MultiTenancy
+{
+    public class MultiTenancyHelper_Test : AbpModuleZeroTestBase
+    {
+        private readonly IMultiTenancyHelper _multiTenancyHelper;
+        public MultiTenancyHelper_Test()
+        {
+            _multiTenancyHelper = Resolve<IMultiTenancyHelper>();
+        }
+
+        [Fact]
+        public void GetTenancyNameByUrl_Test()
+        {
+            var tenancyName = _multiTenancyHelper.SetCurrentTenancy("http://www.cinotam.localhost.com");
+            tenancyName.ShouldBe("cinotam");
+            var tenancyNameWithNoTw = _multiTenancyHelper.SetCurrentTenancy("http://cinotam.localhost.com");
+            tenancyNameWithNoTw.ShouldBe("cinotam");
+            var tenancyNameWithNoPr = _multiTenancyHelper.SetCurrentTenancy("www.cinotam.localhost.com");
+            tenancyNameWithNoPr.ShouldBe("cinotam");
+
+        }
+    }
+}
