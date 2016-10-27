bce3431976118d6bb5085c8f5cae6295362906d7
Alan Torres <temp_peri@hotmail.com>
2016-10-27 13:46:27 -0500

    Corregidas pruebas.

--- Tests/Cinotam.AbpModuleZero.Tests/MultiTenancy/MultiTenancyHelper_Test.cs
+++ Tests/Cinotam.AbpModuleZero.Tests/MultiTenancy/MultiTenancyHelper_Test.cs
@@ -15,27 +15,27 @@ namespace Cinotam.AbpModuleZero.Tests.MultiTenancy
         [Fact]
         public void GetTenancyNameByUrl_Test()
         {
-            var tenancyName = _multiTenancyHelper.SetCurrentTenancy("http://www.cinotam.localhost.com");
+            var tenancyName = _multiTenancyHelper.GetCurrentTenancyName("http://www.cinotam.localhost.com");
             tenancyName.ShouldBe("cinotam");
-            var tenancyNameWithNoTw = _multiTenancyHelper.SetCurrentTenancy("http://cinotam.localhost.com");
+            var tenancyNameWithNoTw = _multiTenancyHelper.GetCurrentTenancyName("http://cinotam.localhost.com");
             tenancyNameWithNoTw.ShouldBe("cinotam");
-            var tenancyNameWithHttps = _multiTenancyHelper.SetCurrentTenancy("https://cinotam.localhost.com");
+            var tenancyNameWithHttps = _multiTenancyHelper.GetCurrentTenancyName("https://cinotam.localhost.com");
             tenancyNameWithHttps.ShouldBe("cinotam");
-            var tenancyNameWithNoPr = _multiTenancyHelper.SetCurrentTenancy("www.cinotam.localhost.com");
+            var tenancyNameWithNoPr = _multiTenancyHelper.GetCurrentTenancyName("www.cinotam.localhost.com");
             tenancyNameWithNoPr.ShouldBe("cinotam");
 
-            var tenancyNameWithNoCrOne = _multiTenancyHelper.SetCurrentTenancy("https://www.cinotam.geronimo.host.wow.com");
+            var tenancyNameWithNoCrOne = _multiTenancyHelper.GetCurrentTenancyName("https://www.cinotam.geronimo.host.wow.com");
             tenancyNameWithNoCrOne.ShouldBe("cinotam");
 
-            var tenancyNameWithNoCrTwo = _multiTenancyHelper.SetCurrentTenancy("https://www.cinotam.com");
+            var tenancyNameWithNoCrTwo = _multiTenancyHelper.GetCurrentTenancyName("https://www.cinotam.com");
             tenancyNameWithNoCrTwo.ShouldBe("cinotam");
-            var tenancyNameWithNoCrThree = _multiTenancyHelper.SetCurrentTenancy("https://www.cinotam.com.mx");
+            var tenancyNameWithNoCrThree = _multiTenancyHelper.GetCurrentTenancyName("https://www.cinotam.com.mx");
             tenancyNameWithNoCrThree.ShouldBe("cinotam");
 
 
-            var testLocal = _multiTenancyHelper.SetCurrentTenancy("cinotam.localhost");
+            var testLocal = _multiTenancyHelper.GetCurrentTenancyName("cinotam.localhost");
             testLocal.ShouldBe("cinotam");
-            var testLocalAlt = _multiTenancyHelper.SetCurrentTenancy("cinotam.localhost:61815/Page/main-frame-cpu-dual-core");
+            var testLocalAlt = _multiTenancyHelper.GetCurrentTenancyName("cinotam.localhost:61815/Page/main-frame-cpu-dual-core");
             testLocalAlt.ShouldBe("cinotam");
 
         }
