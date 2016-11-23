a486ad2865fbd9772aa359826233bd936282721e
Alan Torres <temp_peri@hotmail.com>
2016-11-23 16:40:35 -0600

    Solo algúnas ideas agregadas

---
+++ Cinotam.AbpModuleZero.Tools/Extensions/ClassCheckers.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace Cinotam.AbpModuleZero.Tools.Extensions
+{
+    public static class ClassCheckers
+    {
+        public static IDictionary<string, string> HasAttribute(Type attribute, IEnumerable<PropertyInfo> properties, object obj)
+        {
+            var dic = new Dictionary<string, string>();
+            foreach (var propertyInfo in properties)
+            {
+
+                var equals = propertyInfo.GetCustomAttributes(attribute, false).Length > 0;
+                if (equals)
+                {
+                    dic.Add(propertyInfo.Name, propertyInfo.GetValue(obj).ToString());
+                }
+            }
+            return dic;
+        }
+    }
+}
--- Cinotam.SimplePost.Application/Posts/Dto/NewPostInput.cs
+++ Cinotam.SimplePost.Application/Posts/Dto/NewPostInput.cs
@@ -1,4 +1,5 @@
 ﻿using Abp.Application.Services.Dto;
+using Cinotam.AbpModuleZero.LocalizableContent.Contracts;
 
 namespace Cinotam.SimplePost.Application.Posts.Dto
 {
@@ -8,11 +9,21 @@ namespace Cinotam.SimplePost.Application.Posts.Dto
         public Content Content { get; set; }
     }
 
-    public class Content : EntityDto
+    public class Content
     {
         public string Title { get; set; }
         public string ContentString { get; set; }
         public string Lang { get; set; }
 
+        [IsSharedProperty]
+        public string SharedString { get; set; } = "Shared string";
     }
+
+
+
+    //public class LocalizableContentDto<T> where T : class
+    //{
+    //    public T Content { get; set; }
+    //    public Dictionary<string, string> SharedProps { get; set; }
+    //}
 }
