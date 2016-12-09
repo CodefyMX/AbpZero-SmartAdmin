19e51c66105753a694a05948ad0d7231385f9294
Alan Torres <temp_peri@hotmail.com>
2016-12-09 14:57:32 -0600

    Eliminar usuarios, desbloquear usuarios

--- Cinotam.AbpModuleZero.Web/App/SysAdmin/Main/app.js
+++ Cinotam.AbpModuleZero.Web/App/SysAdmin/Main/app.js
@@ -1,8 +1,31 @@
 ﻿(function () {
     'use strict';
-    window.webConst = {
-         contentFolder:'/App/SysAdmin/Main/modules/web/',
-         
+    var webConst = {
+        contentFolder: '/App/SysAdmin/Main/modules/web/',
+        datatablesLangConfig: {
+            "decimal": "",
+            "emptyTable": App.localize("NoDataAvailableInTable"), //"No hay datos disponibles en la tabla"
+            "info": App.localize("ShowingStartToEnd"),  // "Mostrando _START_ a _END_ de _TOTAL_ entradas",
+            "infoEmpty": App.localize("ZeroEntries"),// "Mostrando 0 a 0 de 0 entradas",
+            "infoFiltered": "",
+            "infoPostFix": "",
+            "thousands": ",",
+            "lengthMenu": App.localize("ShowEntries"), // "Mostrar _MENU_ entradas",
+            "loadingRecords": App.localize("Loading"), //"Cargando...",
+            "processing": App.localize("Processing"), //"Procesando...",
+            "search": App.localize("Search"),// "Buscar:",
+            "zeroRecords": App.localize("NoCoincidences"),// "No hay coincidencias",
+            "paginate": {
+                "first": App.localize("PaginateFirst"),
+                "last": App.localize("PaginateLast"),
+                "next": App.localize("PaginateNext"),
+                "previous": App.localize("PaginateBefore")
+            },
+            "aria": {
+                "sortAscending": ": activate to sort column ascending",
+                "sortDescending": ": activate to sort column descending"
+            }
+        }
     }
     var app = angular.module('app', [
         'ngAnimate',
@@ -14,5 +37,5 @@
         'datatables',
         'datatables.bootstrap',
         'abp'
-    ]).constant('WebConst',window.webConst);
+    ]).constant('WebConst', webConst);
 })();
--- Cinotam.AbpModuleZero.Web/App/SysAdmin/Main/modules/web/users/createEdit.js
+++ Cinotam.AbpModuleZero.Web/App/SysAdmin/Main/modules/web/users/createEdit.js
@@ -1,31 +1,34 @@
-(function() {
-'use strict';
+(function () {
+    'use strict';
 
     angular
         .module('app.web')
         .controller('app.views.users.createEdit', CreateEditController);
 
-    CreateEditController.$inject = ['$uibModalInstance','items','$scope','abp.services.app.user'];
-    function CreateEditController($uibModalInstance,items,$scope,_usersService) {
+    CreateEditController.$inject = ['$uibModalInstance', 'items', '$scope', 'abp.services.app.user'];
+    function CreateEditController($uibModalInstance, items, $scope, _usersService) {
         var vm = this;
         vm.user = {
-            id:0
+            id: 0
         };
         activate();
-        vm.cancel = function (){
+        vm.cancel = function () {
             $uibModalInstance.close();
         }
-        vm.submit = function(form){
-            _usersService.createUser(vm.user).then(function(){
+        vm.submit = function (form) {
+
+            _usersService.createUser(vm.user).then(function () {
                 $uibModalInstance.close("userCreated");
             });
         }
         ////////////////
 
-        function activate() { 
-            if(items.userId){
-                _usersService.getUserForEdit(items.userId).then(function(response){
+        function activate() {
+            if (items.userId) {
+                abp.ui.setBusy();
+                _usersService.getUserForEdit(items.userId).then(function (response) {
                     vm.user = response.data;
+                    abp.ui.clearBusy();
                 })
             }
         }
