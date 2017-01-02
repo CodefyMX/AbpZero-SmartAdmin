0e2710c2cd9e37aa80a841e28369a229300f0e4f
Alan Torres <temp_peri@hotmail.com>
2017-01-02 14:46:39 -0600

    Funcionalidad faltante agregada

--- Cinotam.AbpModuleZero.Web/App/SysAdmin/Main/modules/web/configurations/index.js
+++ Cinotam.AbpModuleZero.Web/App/SysAdmin/Main/modules/web/configurations/index.js
@@ -1,4 +1,4 @@
-(function () {
+(function() {
     'use strict';
 
     angular
@@ -9,6 +9,31 @@
     function ConfigurationsController(_settingsService) {
         var vm = this;
         //Define the configs 
+        var isBoolean = function(string) {
+            console.log(string);
+            if (string == 'true' || string === true) {
+
+                return true;
+            }
+            else {
+                if (string == 'false' || string === false) {
+                    return true;
+                }
+                return false;
+            }
+        }
+        vm.isBoolean = function(settingValue) {
+            var result = isBoolean(settingValue);
+            if (result) {
+                console.log(settingValue, 'is boolean');
+                return true;
+            }
+            else {
+                console.log(settingValue, 'is not boolean');
+                return false;
+            }
+        }
+
         vm.configurationRequests = [
             {
                 settingNames: [
@@ -60,47 +85,55 @@
 
         function activate() {
             // lol !! async madness
-            vm.configurationRequests.forEach(function (element) {
+            vm.configurationRequests.forEach(function(element) {
                 configurations = [];
-                element.settingNames.forEach(function (setting) {
+                element.settingNames.forEach(function(setting) {
                     configurations.push(setting);
                 });
-                _settingsService.getSettingsOptions(configurations).then(function (response) {
+                _settingsService.getSettingsOptions(configurations).then(function(response) {
                     //Build tabs
                     var newFormElement = new formElement(element.configurationName);
-                    response.data.settings.forEach(function (settingElement) {
+                    response.data.settings.forEach(function(settingElement) {
                         newFormElement.addSetting(settingElement);
                     });
                     vm.configForms.push(newFormElement);
                 });
             });
         }
-        vm.submit = function () {
+        vm.submit = function() {
             var data = [];
-            vm.configForms.forEach(function (config) {
-                config.settings.forEach(function (elm) {
+            vm.configForms.forEach(function(config) {
+                config.settings.forEach(function(elm) {
                     data.push({
                         key: elm.key,
                         value: elm.value,
                         settingScopes: elm.settingScopes
                     });
                 });
+                console.log(data);
             });
-            _settingsService.createEditSetting(data).then(function(){
+            _settingsService.createEditSetting(data).then(function() {
                 abp.notify.success(App.localize("ChangesSaved"), App.localize("Success"));
             });
         }
-        vm.clearCaches = function () {
-            _settingsService.clearCaches().then(function () {
+        vm.clearCaches = function() {
+            _settingsService.clearCaches().then(function() {
                 abp.notify.success(App.localize("CacheCleaned"), App.localize("Success"));
             });
         }
-        var formElement = function (name, settings) {
+        var formElement = function(name, settings) {
             this.name = name;
             this.settings = [];
 
-            this.addSetting = function (setting) {
-                this.settings.push(setting);
+            this.addSetting = function(setting) {
+                var isBoolean = vm.isBoolean(setting.value);
+                if (isBoolean) {
+                    setting.value = (setting.value === 'true');
+                    this.settings.push(setting);
+                }
+                else {
+                    this.settings.push(setting);
+                }
             }
             return this;
         }
