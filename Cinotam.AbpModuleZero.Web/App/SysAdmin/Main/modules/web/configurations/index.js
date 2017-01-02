22dd9b657f503574ef0ef8c61a5b72ba7d0c1bc4
Alan Torres <temp_peri@hotmail.com>
2017-01-02 10:47:12 -0600

    Página para configuración agregada -Los tabs aparecen desordenados, async madness

---
+++ Cinotam.AbpModuleZero.Web/App/SysAdmin/Main/modules/web/configurations/index.js
@@ -0,0 +1,109 @@
+(function () {
+    'use strict';
+
+    angular
+        .module('app.web')
+        .controller('app.views.configurations.index', ConfigurationsController);
+
+    ConfigurationsController.$inject = ['abp.services.app.settings'];
+    function ConfigurationsController(_settingsService) {
+        var vm = this;
+        //Define the configs 
+        vm.configurationRequests = [
+            {
+                settingNames: [
+                    "Abp.Net.Mail.Smtp.Host",
+                    "Abp.Net.Mail.Smtp.Port",
+                    "Abp.Net.Mail.Smtp.UserName",
+                    "Abp.Net.Mail.Smtp.Password",
+                    "Abp.Net.Mail.Smtp.Domain",
+                    "Abp.Net.Mail.Smtp.EnableSsl",
+                    "Abp.Net.Mail.Smtp.UseDefaultCredentials",
+                    "Abp.Net.Mail.DefaultFromAddress",
+                    "Abp.Net.Mail.DefaultFromDisplayName"
+                ],
+                configurationName: App.localize('EmailSettings')
+            },
+            {
+                settingNames: [
+                    "Abp.Localization.DefaultLanguageName",
+                    "Abp.Timing.TimeZone",
+                    "Abp.Zero.OrganizationUnits.MaxUserMembershipCount",
+                    "Abp.WebApi.Runtime.Caching.ClearPassword",
+                    "WebSiteStatus",
+                    "UseSmtp",
+                    "AppMode"
+                ],
+                configurationName: App.localize('Other')
+            },
+            {
+                settingNames: [
+                    "Abp.Zero.UserManagement.IsEmailConfirmationRequiredForLogin",
+                    "Abp.Zero.UserManagement.TwoFactorLogin.IsEnabled",
+                    "Abp.Zero.UserManagement.TwoFactorLogin.IsRememberBrowserEnabled",
+                    "Abp.Zero.UserManagement.TwoFactorLogin.IsEmailProviderEnabled",
+                    "Abp.Zero.UserManagement.TwoFactorLogin.IsSmsProviderEnabled",
+                    "Abp.Zero.UserManagement.UserLockOut.IsEnabled",
+                    "Abp.Zero.UserManagement.UserLockOut.MaxFailedAccessAttemptsBeforeLockout",
+                    "Abp.Zero.UserManagement.UserLockOut.DefaultAccountLockoutSeconds",
+                    ""
+                ],
+                configurationName: App.localize('Account')
+            },
+
+        ]
+        var configurations = [];
+        vm.configForms = [];
+        activate();
+
+        ////////////////
+
+        function activate() {
+            // lol !! async madness
+            vm.configurationRequests.forEach(function (element) {
+                configurations = [];
+                element.settingNames.forEach(function (setting) {
+                    configurations.push(setting);
+                });
+                _settingsService.getSettingsOptions(configurations).then(function (response) {
+                    //Build tabs
+                    var newFormElement = new formElement(element.configurationName);
+                    response.data.settings.forEach(function (settingElement) {
+                        newFormElement.addSetting(settingElement);
+                    });
+                    vm.configForms.push(newFormElement);
+                });
+            });
+        }
+        vm.submit = function () {
+            var data = [];
+            vm.configForms.forEach(function (config) {
+                config.settings.forEach(function (elm) {
+                    data.push({
+                        key: elm.key,
+                        value: elm.value,
+                        settingScopes: elm.settingScopes
+                    });
+                });
+            });
+            _settingsService.createEditSetting(data).then(function(){
+                abp.notify.success(App.localize("ChangesSaved"), App.localize("Success"));
+            });
+        }
+        vm.clearCaches = function () {
+            _settingsService.clearCaches().then(function () {
+                abp.notify.success(App.localize("CacheCleaned"), App.localize("Success"));
+            });
+        }
+        var formElement = function (name, settings) {
+            this.name = name;
+            this.settings = [];
+
+            this.addSetting = function (setting) {
+                this.settings.push(setting);
+            }
+            return this;
+        }
+
+    }
+})();
